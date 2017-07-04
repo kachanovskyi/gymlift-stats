252f655c433090498e5ec45ad6213f35fe890c82
Walter Kachanovsky <walterkachanovsky@Walters-MacBook-Pro.local>
2017-07-04 12:10:35 +0300

    returned link back to local

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
     function loadExercises(date) {
         $.ajax({
             type: "POST",
-            url: 'https://rawgit.com/kachanovskyi/gymlift-stats/master/data/exercises.json',
+            url: './data/exercises.json',
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             data: JSON.stringify(date),
